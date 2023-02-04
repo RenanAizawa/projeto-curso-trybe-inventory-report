6d76778ed23b52b7b246cf6fb2f4cde4d95534fd
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2023-02-04 19:39:34 -0400

    [feat]: adicionado funcionalidades para detecção de arquivos csv, json e xml, bem como tipo de relatorio desejado

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,22 @@
+from inventory_report.inventory.worked_data import WorkedData
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class Inventory:
+    @staticmethod
+    def relatorio_type(data_list: list, type: str):
+        if type == "simples":
+            return SimpleReport.generate(data_list)
+        return CompleteReport.generate(data_list)
+ 
+    def import_data(path: str, type: str):
+        if '.csv' in path:
+            resposta = WorkedData.clean_csv(path)
+            return Inventory.relatorio_type(resposta, type)
+        elif '.json' in path:
+            resposta = WorkedData.clean_json(path)
+            return Inventory.relatorio_type(resposta, type)
+        elif '.xml' in path:
+            resposta = WorkedData.clean_xml(path)
+            return Inventory.relatorio_type(resposta, type)
---
+++ inventory_report/inventory/worked_data.py
@@ -0,0 +1,25 @@
+import csv
+import json
+import xmltodict
+
+
+class WorkedData:
+    @staticmethod
+    def clean_csv(path:str):
+        with open(path) as data:
+            changed = csv.DictReader(data)
+            listed_changed = list(changed)
+        return listed_changed
+
+    @staticmethod
+    def clean_json(path:str):
+        with open(path) as data:
+            changed = data.read()
+            listed_changed = list(json.loads(changed))
+        return listed_changed
+
+    @staticmethod
+    def clean_xml(path:str):
+        with open(path) as file:
+            changed = xmltodict.parse(file.read())["dataset"]["record"]
+        return changed
