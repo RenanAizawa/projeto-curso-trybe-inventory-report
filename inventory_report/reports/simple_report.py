68a0cb62235cd1c1382f7d8667c556622433a3a7
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2023-02-04 14:17:02 -0400

    [feat]: implementação do metodo generate na classe SimpleReport

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,15 @@
+from inventory_report.reports.tool_report import Tools_report
+
 class SimpleReport:
-    pass
+    def __init__(self):
+        self.tools_report = Tools_report()
+
+
+    def generate(self, data_list):
+        return (
+            f"Data de fabricação mais antiga: {self.tools_report.oldest_fabric_date(data_list)}\n"
+            f"Data de validade mais próxima: {data_val_mais_proxiself.tools_report.closet_valid_date(data_list)}\n"
+            f"Empresa com mais produtos: {self.tools_report.most_products(data_list)}\n"
+        )
+
+    def 
---
+++ inventory_report/reports/tools_report.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+class Tools_report:
+    def most_products(data_list: list):
+        company_count = {}
+        for company in data_list:
+            if company["nome_da_empresa"] not in company_count.keys():
+                company_count[f"{company["nome_da_empresa"]}"] = 1
+            else:
+                company_count[f"{company["nome_da_empresa"]}"] += 1
+        return max(company_count)
+
+    def closet_valid_date(data_list: list):
+        today = datetime.now()
+        datas_validas = []
+        for product in data_list:
+            data_validade = datetime.strptime(product["data_de_validade"], format_date)
+            if data_validade > today:
+                datas_validas.append(data_validade)
+        return  min(datas_validas).date()
+
+
+    
+    def oldest_fabric_date(data_list: list):
+        oldest_date = datetime.now()
+        for product in data_list:
+            fabric_date = datetime.strptime(product["data_de_fabricacao"], format_date)
+            if fabric_date < oldest_date:
+                oldest_date = fabric_date
+        return  oldest_date.date()
+    
+
+    def company_stock(data_list: list):
+        stock_dict = calculate_stock_company(data_list)
+        return "".join(stock_dict)
+    
+
+    def calculate_stock_company(data_list: list):
+        pass
+
+
+    format_date = "%y-%m-%d"
